d8fa3a551127cd453907171134f40ef572d61a78
Barak Shirali <barak.shirali@gmail.com>
2017-10-24 17:00:38 +0100

    fix(webpack): webpack assets js fix

--- tools/webpack/config.babel.js
+++ tools/webpack/config.babel.js
@@ -5,7 +5,6 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const BabiliPlugin = require('babili-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
 
 const nodeEnv = process.env.NODE_ENV || 'development';
 const isDev = nodeEnv !== 'production';
@@ -84,14 +83,6 @@ const getPlugins = () => {
       new webpack.HashedModuleIdsPlugin(),
       new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', minChunks: Infinity }),
       new webpack.optimize.ModuleConcatenationPlugin(),
-      new CompressionPlugin({
-        asset: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: /\.jsx?$|\.css$|\.(scss|sass)$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.8,
-        deleteOriginalAssets: true,
-      }) // eslint-disable-line comma-dangle
     );
   }
 
